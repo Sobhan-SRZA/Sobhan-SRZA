281f54b40ecc821246ecbd8b143eaefdd786a042
Sobhan <sobhan.rasoulzade@gmail.com>
2025-05-10 00:28:27 +0330

    update

--- generateReposTable.js
+++ generateReposTable.js
@@ -1,37 +1,4 @@
-import fetch from 'node-fetch';
 import fs from 'fs';
-
-const GITHUB_USERNAME = process.env.GITHUB_USERNAME || 'YOUR_GITHUB_USERNAME';
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN || 'YOUR_GITHUB_TOKEN';
-
-async function fetchRepos(username) {
-  const repos = [];
-  let page = 1;
-  const perPage = 100;
-  let fetchedData = [];
-
-  do {
-    const apiUrl = `https://api.github.com/users/${username}/repos?per_page=${perPage}&page=${page}`;
-    const response = await fetch(apiUrl, {
-      headers: {
-        'Authorization': `token ${GITHUB_TOKEN}`,
-        'Accept': 'application/vnd.github.v3+json'
-      }
-    });
-
-    if (!response.ok) {
-      console.error(`fetch error: ${response.status} ${response.statusText}`);
-      break;
-    }
-
-    fetchedData = await response.json();
-    repos.push(...fetchedData);
-    console.log(fetchedData);
-    page++;
-  } while (fetchedData.length === perPage);
-
-  return repos;
-}
 
 async function generateMarkdownTable(repos) {
   let table = '| 🔢 | 🗃 Projects | 📖 Describe | 📡 Status | 🔐 Access | 🌎 Language | ⚙️ Technology | ⭐ Stars | 🖨 Forks |\n';
@@ -70,6 +37,8 @@ async function main() {
     console.log(`repositorise size: ${repos.length}`);
     const markdownTable = await generateMarkdownTable(repos);
     fs.writeFileSync('REPOSITORIES.md', markdownTable);
+    const persianCaesarMarkdownTable = await generateMarkdownTable(repos.filter(a => a.organization === "Persian-Caesar"));
+    fs.writeFileSync('PC_REPOSITORIES.md', persianCaesarMarkdownTable);
     console.log('REPOSITORIES.md has successfully created.');
   } catch (error) {
     console.error('get an error:', error);
